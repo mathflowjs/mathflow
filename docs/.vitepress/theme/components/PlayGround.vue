7f36b08e28e0be92e987c524cbba611ee626e769
Henry Hale <henryhale@users.noreply.github.com>
2024-01-07 14:25:44 +0300

    docs(fix): playground shareable link

--- docs/.vitepress/theme/components/PlayGround.vue
+++ docs/.vitepress/theme/components/PlayGround.vue
@@ -2,13 +2,15 @@
 import { ref, onMounted } from 'vue';
 import { evaluate } from '../../../../dist/index.mjs';
 
+const input = ref('');
 const code = ref('');
 const output = ref('');
 const link = ref('');
+const copied = ref(false);
 
 function runScript() {
     try {
-        output.value = JSON.stringify(evaluate(code.value));
+        output.value = JSON.stringify(evaluate(code.value), null, 2);
     } catch (error) {
         output.value = '' + error;
     }
@@ -19,7 +21,7 @@ function generateURL() {
         window.location.origin +
         window.location.pathname +
         '?s=' +
-        encodeURI(code.value);
+        encodeURIComponent(code.value);
 }
 
 function reset() {
@@ -27,13 +29,22 @@ function reset() {
     output.value = '';
 }
 
+function copyURL() {
+    try {
+        navigator.clipboard.writeText(link.value);
+        copied.value = true;
+        setTimeout(() => {
+            copied.value = false;
+        }, 100);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 onMounted(() => {
-    const search = window.location.search;
-    const raw = search.substring(search.lastIndexOf('=') + 1);
-    const script = decodeURI(raw);
-    console.log(script);
-    if (script.length) {
-        code.value = script;
+    const params = new URLSearchParams(window.location.search);
+    if (params.has('s')) {
+        code.value = decodeURIComponent(params.get('s'));
     }
 });
 </script>
@@ -43,7 +54,7 @@ onMounted(() => {
         <div>
             <label for="code">Code</label>
             <br />
-            <textarea v-model.trim="code" @input="reset" name="code" id="code" spellcheck="false"
+            <textarea ref="input" v-model.trim="code" @input="reset" name="code" id="code" spellcheck="false"
                 autocomplete="off"></textarea>
         </div>
         <div>
@@ -51,7 +62,11 @@ onMounted(() => {
             <button @click="generateURL" :disabled="!code">Share</button>
         </div>
         <div v-show="code.length && link.length">
-            <b>Link:</b> {{ link }}
+            <div class="copy">
+                <span>Link</span>
+                <span v-text="link"></span>
+                <span @click="copyURL">{{ copied ? 'Copied' : 'Copy' }}</span>
+            </div>
         </div>
         <div v-show="output.length">
             <b>Output:</b>
@@ -100,5 +115,39 @@ textarea {
     overflow-wrap: break-word;
     border-radius: 8px;
     min-height: 250px;
+}
+
+.copy {
+    display: flex;
+    border: 1px solid var(--vp-input-border-color);
+    border-radius: 10px;
+    overflow: hidden;
+    transition: all 0.25ms;
+}
+
+.copy>span {
+    display: inline-block;
+    padding: 5px 15px;
+    max-height: 50px;
+}
+
+.copy>span:nth-child(2) {
+    flex-grow: 1;
+    border-width: 0 1px 0 1px;
+    border-style: solid;
+    border-color: var(--vp-input-border-color);
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+
+.copy>span:last-child {
+    background-color: var(--vp-input-switch-bg-color);
+    cursor: pointer;
+}
+
+.copy>span:last-child:hover {
+    background-color: var(--vp-bg-brand);
+    color: var(--vp-c-brand-1);
 }
 </style>
