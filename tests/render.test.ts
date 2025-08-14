1a7c501d0b339b088d337985cc2221fc2a6d1c82
Henry Hale <92443116+henryhale@users.noreply.github.com>
2025-08-14 09:38:09 +0300

    test: add latex output test

---
+++ tests/render.test.ts
@@ -0,0 +1,24 @@
+import { describe, test, expect, beforeEach } from 'vitest';
+import { type ContextAPI, createContext } from '../src/context';
+
+let ctx: ContextAPI;
+
+const expr = '1 + 3sin(30)';
+
+beforeEach(() => {
+    ctx = createContext();
+});
+
+describe('latex output', () => {
+    test('mode of output', () => {
+        expect(ctx.renderAsLaTeX(expr, { mode: 'inline' })).toEqual(
+            '$1+3 \\cdot \\text{sin}\\left(30\\right) \\\\ $'
+        );
+        expect(ctx.renderAsLaTeX(expr, { mode: 'display' })).toEqual(
+            '$$1+3 \\cdot \\text{sin}\\left(30\\right) \\\\ $$'
+        );
+        expect(ctx.renderAsLaTeX(expr, { mode: 'align' })).toEqual(
+            `\\begin{align}\n1+3 \\cdot \\text{sin}\\left(30\\right) \\\\\n\n\\end{align}`
+        );
+    });
+});
